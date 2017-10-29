6d99a91723d1e18664a14103ae63cb91728df164
Phil <phil@brainlabsdigital.com>
2017-10-29 08:05:36 +0000

    added state template

---
+++ bullet-journal-app/state.js
@@ -0,0 +1,12 @@
+var state = {
+    dateFocus : Date,
+    items : [
+        {
+            _id : 0,
+            title : "example item",
+            dates : [Date, Date, Date],
+            parents : [page1, page2],
+            child : page3
+        }
+    ]
+}
