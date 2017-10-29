b10995b94abc325750f16729e26f7a2b7e2766e7
Phil <phil@brainlabsdigital.com>
2017-10-29 08:05:11 +0000

    added redux

--- README.md
+++
@@ -1,4 +0,0 @@
-# Bullet Journal
-A  dynamic planner based off the bullet journal methodology.
-
-Create modular collections of periods of time or just places to store ideas
--- app/sample-data.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'items' : [
-        {_id : 0, text : "hello"}, 
-        {_id : 1, text : "hi"}, 
-        {_id : 2, text : "bye"}, 
-        {_id : 3, text : "goodbye"}, 
-    ]
-}
